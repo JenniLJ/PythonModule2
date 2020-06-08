55ec027bb80417d1503677bc080658b8f6939e76
Jenni Jarrell <48532943+JenniLJ@users.noreply.github.com>
2020-06-08 18:56:40 -0500

    camper age year to month conversion

---
+++ camper_age_input.py
@@ -0,0 +1,7 @@
+def convert_to_months(x):
+    pass
+
+if __name__ == '__main__':
+    x = input("Enter an age:")
+    answer = convert_to_months(int(x))
+    print(answer)
---
+++ test_functions.py
@@ -0,0 +1,10 @@
+import unittest
+from main import camper_age_input
+
+class FunctionTestCase(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(24, camper_age_input.convert_to_months(2))
+
+
+if __name__ == '__camper_age_input__':
+    unittest.main()
