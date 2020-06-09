3d3c06dcbe1133019278f6c82c66b9494a597c8a
JenniLJ <48532943+JenniLJ@users.noreply.github.com>
2020-06-08 19:22:28 -0500

    finished function code and test

--- test/test_functions.py
+++ test/test_functions.py
@@ -1,8 +1,9 @@
 import unittest
 from main import camper_age_input
 
+
 class FunctionTestCase(unittest.TestCase):
-    def test_something(self):
+    def test_int2(self):
         self.assertEqual(24, camper_age_input.convert_to_months(2))
 
 
