b3824e0903e3c06ed42d9be3fb3c5fe2b899a75a
JenniLJ <48532943+JenniLJ@users.noreply.github.com>
2020-06-08 19:46:30 -0500

    finished function code and test

--- main/camper_age_input.py
+++ main/camper_age_input.py
@@ -1,7 +1,27 @@
+"""
+
+Program: camper_age_input.py
+
+Author: Jenni Jarrell
+
+Last date modified: 06/08/2020
+
+
+The purpose of this program is to take an input of an age in years and convert it to months.
+This is also to practice unit testing.
+
+
+"""
+
 def convert_to_months(x):
     return x*12
 
+
 if __name__ == '__main__':
-    x = input("Enter an age:")
-    answer = convert_to_months(int(x))
-    print(answer)
+    # user input
+    age_in_years = input("Enter an age in whole years:")
+    # declare and initialize variable
+    # call function and save result to variable
+    age_in_months = convert_to_months(int(age_in_years))
+    print(str(age_in_years) + " years is " + str(age_in_months) + " months.")
+# this is a comment for submission
